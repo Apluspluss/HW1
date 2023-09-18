93695ce4d1b9c6458056da57d4ea93250e396550
Antimony8 <97124775+Antimony8@users.noreply.github.com>
2023-09-18 16:17:57 -0400

    Update Binary_Search.py

--- Binary_Search.py
+++ Binary_Search.py
@@ -13,3 +13,30 @@ def Recursive_Binary_Search(A, x, low, high):
         return Recursive_Binary_Search(A, x, mid + 1, high)
     else:
         return Recursive_Binary_Search(A, x, low, mid - 1)
+
+# Function to measure the runtime of the binary search
+def measure_runtime():
+    sizes = []  # List to store input array sizes
+    runtimes = []  # List to store corresponding runtimes
+
+    for size in range(1, 10001, 100):  # Vary the input size from 1 to 10,000
+        input_array = list(range(1, size + 1))  # Generate a sorted input array
+
+        start_time = time.time()  # Record the start time
+        target_index = Recursive_Binary_Search(input_array, size // 2, 0, size - 1)
+        end_time = time.time()  # Record the end time
+
+        elapsed_time = end_time - start_time  # Calculate the elapsed time
+        sizes.append(size)
+        runtimes.append(elapsed_time)
+
+    return sizes, runtimes
+
+# Measure runtime and create a plot
+sizes, runtimes = measure_runtime()
+plt.plot(sizes, runtimes)
+plt.xlabel("Input Array Size")
+plt.ylabel("Runtime (seconds)")
+plt.title("Runtime Analysis of Recursive Binary Search")
+plt.grid()
+plt.show()
